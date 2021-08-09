f298e3ed3ee340d6efbb927fd4a69019f89b5a97
byunggyukim <needpia@naver.com>
2021-08-09 23:55:26 +0900

    save

---
+++ src/main/java/service/post/PostsService.java
@@ -0,0 +1,20 @@
+package service.post;
+
+import com.stutdy.book.domain.posts.PostsRepository;
+import com.stutdy.book.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity())
+                .getId();
+    }
+}
